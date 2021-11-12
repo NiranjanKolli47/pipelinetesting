fdffda9139bd2e13de2187da3f9abe5981a587eb
Niranjan <Niranjankumarkolli@outlook.com>
2021-11-12 21:39:19 +0530

    this is pipeline code to pull from git through Jenkins

--- echo.bat
+++ echo.bat
@@ -6,5 +6,7 @@ Rem Turns the echo off so that each command will not be shown when executed
 @echo off 
 echo "Hello World" 
 
+echo "This is your first instance of pulling code from Git through Jenkins"
 Rem Displays the contents of the PATH variable 
 echo %PATH%
+
