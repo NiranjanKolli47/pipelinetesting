a2a0ee1b606f319c7dc7bfccdac11bd3d30d5337
NiranjanKolli47 <94030899+NiranjanKolli47@users.noreply.github.com>
2021-11-17 16:28:25 +0530

    Update echo.bat

--- echo.bat
+++ echo.bat
@@ -1,15 +1,15 @@
-Rem Turns the echo on so that each command will be shown as executed 
+--Rem Turns the echo on so that each command will be shown as executed 
 echo on 
 echo "Hello World" 
 
-Rem Turns the echo off so that each command will not be shown when executed 
+--Rem Turns the echo off so that each command will not be shown when executed 
 @echo off 
 echo "Hello World" 
 
 echo %Date%
 echo "This is your first instance of pulling code from Git through Jenkins"
 
-Rem Displays the contents of the PATH variable 
+--Rem Displays the contents of the PATH variable 
 echo %PATH%
 
 Copy "H:\Downloads back up on 18-06-21\evt_api_event_pkg.sql" G:\
