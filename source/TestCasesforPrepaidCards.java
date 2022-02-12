5d9535c973a36ee623f3ae1f9c8fe4c2bd3fb5a9
Niranjan <Niranjankumarkolli@outlook.com>
2022-02-12 06:19:09 +0530

    TestCasesforPrepaidCards.java

---
+++ source/TestCasesforPrepaidCards.java
@@ -0,0 +1,13 @@
+public class TestCasesforPrepaidCard {
+
+
+	public void PrepaidCardLogin()
+	{
+		System.out.println("This is to test for PrepaidCardLogin");
+	}
+
+	public void PrepaidCardTxn()
+	{
+		System.out.println("This is to test for PrepaidCardTxn");
+	}
+}
