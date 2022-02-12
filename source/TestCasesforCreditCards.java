84342d109f1b8902bd7ec508b259790e7e90405b
Niranjan <Niranjankumarkolli@outlook.com>
2022-02-12 07:20:02 +0530

    Test java project

---
+++ source/TestCasesforCreditCards.java
@@ -0,0 +1,19 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class TestCasesforCreditCards {
+	@Test
+	public void CreditCardLogin()
+	{
+		System.out.println("This is to test for CreditCardLogin");
+	}
+	
+	@Test
+	public void CreditCardTxn()
+	{
+		System.out.println("This is to test for CreditCardTxn");
+	
+	}
+
+}
