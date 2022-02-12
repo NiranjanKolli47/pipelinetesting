2a6fedce128b647e7621328a72ae994004f26249
Niranjan <Niranjankumarkolli@outlook.com>
2022-02-12 07:19:40 +0530

    Test java project

---
+++ source/TestCasesforClosedLoopCards.java
@@ -0,0 +1,20 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class TestCasesforClosedLoopCards {
+	
+	@Test
+	public void PrepaidCardLogin()
+	{
+		System.out.println("This is to test for PrepaidCardLogin");
+	}
+
+	@Test
+	public void PrepaidCardTxn()
+	{
+		System.out.println("This is to test for PrepaidCardTxn");
+	}
+
+
+}
---
+++ source/TestCasesforDebitCards.java
@@ -0,0 +1,18 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class TestCasesforDebitCards {
+
+	@Test
+	public void DebitCardLogin()
+	{
+		System.out.println("This is to test for DebitardLogin");
+	}
+	
+	@Test
+	public void DebitCardTxn()
+	{
+		System.out.println("This is to test for DebitCardTxn");
+	}
+}
